7444fad24155dc3acff3496218a17d17df5ced90
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-07-31 03:06:09 +0700

    feat: add sample code on how to use middlewares

--- backend/index.php
+++ backend/index.php
@@ -2,22 +2,30 @@
 
 require_once(__DIR__ . '/db/Database.php');
 require_once(__DIR__ . '/./request/Request.php');
+require_once(__DIR__ . '/./response/Response.php');
 require_once(__DIR__ . '/./router/Router.php');
 
 $db = new Database();
 $db->open_connection();
 $db->close_connection();
 
-$router = new Router(new Request);
+$router = new Router(new Request, new Response);
 
-$router->get('/', function () {
-  return '<h1>Hello, Apollo</h1>';
+$router->post('/', function (Request $req, Response $res, $next) {
+  if ($req->get_body()->id != 1)return $res->json()->unauthorized();
+  call_user_func($next);
+}, function (Request $req, Response $res) {
+  return $res->json(['id' => 1, 'name' => 'Tommy Salim', 'age' => 20])->ok();
 });
 
-$router->get('/fetch', function () {
-  return json_encode(['status' => 200, 'message' => 'success']);
+$router->post('/post', function (Request $req, Response $res) {
+  return $res->json($req->get_body())->ok();
 });
 
-$router->post('/echo-self', function (Request $request) {
-  return json_encode(['status' => 200, 'data' => $request->get_body()]);
+$router->put('/put', function (Request $req, Response $res) {
+  return $res->json($req->get_body())->ok();
+});
+
+$router->delete('/delete', function (Request $req, Response $res) {
+  return $res->json($req->get_body())->ok();
 });
