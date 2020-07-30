5bcfe27e3420971e2b0e57413774c70fdec66128
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-07-30 16:48:31 +0700

    refactorL change file naming, maintain consistency
    Changing the naming of a file based on file naming convention.

--- backend/models/BaseModel.php
+++ backend/models/BaseModel.php
@@ -1,16 +1,19 @@
 <?php
 
 require_once(__DIR__ . '/../db/Database.php');
-require_once(__DIR__ . '/../utils/query_builder.php');
+require_once(__DIR__ . '/../utils/QueryBuilder.php');
 
 class Model {
 
   private $table;
   private $db = null;
+  private $queryBuilder = null;
   
   protected function __construct($table) {
     $this->db = new Database();
     $this->db->open_connection();
+
+    $this->queryBuilder = new QueryBuilder();
 
     $this->table = $table;
   }
@@ -21,14 +24,14 @@ class Model {
   }
 
   protected function insert($types, $data) {
-    $query = insert_query_builder($this->table, array_keys($data));
+    $query = $this->queryBuilder->insert_query_builder($this->table, array_keys($data));
     $this->db->prepare($query);
     $this->db->bind($types, array_values($data));
     $this->db->execute();
   }
 
   protected function update($types, $data){
-    $query = update_query_builder($this->table, array_keys($data));
+    $query = $this->queryBuilder->update_query_builder($this->table, array_keys($data));
     $this->db->prepare($query);
     $this->db->bind($types, array_values($data));
     $this->db->execute();
