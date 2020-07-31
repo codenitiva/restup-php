a4641ee1ddcc4bc687428d517f967501b2499830
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-08-01 00:51:51 +0700

    feat: implemented sample app on index.php

--- backend/index.php
+++ backend/index.php
@@ -1,31 +1,20 @@
 <?php
 
-require_once(__DIR__ . '/db/Database.php');
-require_once(__DIR__ . '/./request/Request.php');
-require_once(__DIR__ . '/./response/Response.php');
-require_once(__DIR__ . '/./router/Router.php');
+require(__DIR__ . '/requirements.php');
+
+use Codenitiva\PHP\Application;
+use Codenitiva\PHP\Routers\SampleRouter;
 
 $db = new Database();
 $db->open_connection();
 $db->close_connection();
 
-$router = new Router(new Request, new Response);
+// ? How to use
+// ? Step 1: create a new REST API Application instance
+$app = new Application;
 
-$router->post('/', function (Request $req, Response $res, $next) {
-  if ($req->get_body()->id != 1)return $res->json()->unauthorized();
-  call_user_func($next);
-}, function (Request $req, Response $res) {
-  return $res->json(['id' => 1, 'name' => 'Tommy Salim', 'age' => 20])->ok();
-});
+// ? Step 2: add a sub router using add_sub_router method
+$app->add_sub_router(new SampleRouter);
 
-$router->post('/post', function (Request $req, Response $res) {
-  return $res->json($req->get_body())->ok();
-});
-
-$router->put('/put', function (Request $req, Response $res) {
-  return $res->json($req->get_body())->ok();
-});
-
-$router->delete('/delete', function (Request $req, Response $res) {
-  return $res->json($req->get_body())->ok();
-});
+// ? Step 3: dont't forget to call the serve method to make sure the app is up and running.
+$app->serve();
