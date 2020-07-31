d4abf40e6953c073a558960c201e8bd90e520380
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-08-01 00:49:33 +0700

    feat: update main router class to support middlewares

--- backend/router/Router.php
+++ backend/router/Router.php
@@ -1,19 +1,24 @@
-<?php
+<?php namespace Codenitiva\PHP\Routers;
 
-require_once(__DIR__ . '/../request/Request.php');
-require_once(__DIR__ . '/../response/Response.php');
-require_once(__DIR__ . '/../middleware/Middleware.php');
+use Codenitiva\PHP\Requests\Request;
+use Codenitiva\PHP\Responses\Response;
+use Codenitiva\PHP\Middlewares\Middleware;
+use Codenitiva\PHP\Exceptions\UnknownRouterMethodException;
+use Codenitiva\PHP\Exceptions\UnknownRouteException;
+use Codenitiva\PHP\Configs\AppConfig;
 
 class Router {
   
   private $request;
   private $response;
-  private $middlewares;
+  private $middlewares = [];
+  private $prefix_path;
   private $http_methods = ['GET', 'POST', 'PUT', 'DELETE'];
 
-  public function __construct(Request $request, Response $response) {
+  public function __construct(Request $request, Response $response, $prefix_path = '') {
     $this->request = $request;
     $this->response = $response;
+    $this->prefix_path = $prefix_path;
   }
 
   public function __call($name, $args) {
@@ -21,14 +26,12 @@ class Router {
     count($args) == 3 ? list($route, $middleware, $method) = $args : list($route, $method) = $args;
 
     if (!in_array(strtoupper($name), $this->http_methods)) {
-      throw new Exception("Undefined method $name");
+      throw new UnknownRouterMethodException($name);
     }
 
-    // TODO ! remove /apollo/backend on production mode
-    $this->{strtolower($name)}['/apollo/backend' . $this->format_route($route)] = $method;
+    $this->{strtolower($name)}[$this->format_route($route)] = $method;
     if ($middleware != null) {
-      $this->middlewares[strtolower($name) . '/apollo/backend' . $this->format_route($route)] = 
-        new Middleware($this->request, $this->response, $middleware);
+      $this->middlewares[$this->append_method_to_route($this->format_route($route))] = $middleware;
     }
   }
 
@@ -38,36 +41,44 @@ class Router {
 
   private function resolve() {
     $method_dict = $this->{strtolower($this->request->request_method)};
-    $formated_route = $this->format_route($this->request->request_uri);
+    $formated_route = $this->clean_route($this->request->request_uri);
 
     if (!array_key_exists($formated_route, $method_dict)) {
-      echo 'Unknown route: <b>' . $this->request->request_method . '</b> ' . $formated_route;
-      return;
+      throw new UnknownRouteException($this->request->request_method, $formated_route);
     }
 
-    if ($this->run_middleware($formated_route)) {
+    if ($this->run_middleware($formated_route) == Middleware::NEXT) {
       $method = $method_dict[$formated_route];
-      echo call_user_func_array($method, array($this->request, $this->response));
+      $this->run_controller($method);
     }
   }
 
   private function run_middleware($route) {
-    $middleware_route = strtolower($this->request->request_method) . $route;
+    $middleware_route = $this->append_method_to_route($route);
     if (array_key_exists($middleware_route, $this->middlewares)) {
       $middleware = $this->middlewares[$middleware_route];
-      $middleware_output = $middleware->run();
-      if ($middleware_output != 'pass') {
-        echo $middleware_output;
-        return false;
-      }
+      return $middleware->run($this->request, $this->response);
     }
-    return true;
+    return Middleware::NEXT;
+  }
+
+  private function run_controller($method) {
+    echo call_user_func_array($method, array($this->request, $this->response));
+  }
+
+  private function clean_route($route) {
+    $clean_route = str_replace('/index.php', '', trim($route));
+    $clean_route = str_replace(AppConfig::PATH_FROM_ROOT, '', $clean_route);
+    if ($clean_route == '' || substr($clean_route, -1) != '/') $clean_route .= '/';
+    return $clean_route;
   }
 
   private function format_route($route) {
-    $clean_route = str_replace('/index.php', '', trim($route));
-    if ($clean_route == '') return '/';
-    if (substr($clean_route, -1) != '/') return "$clean_route/";
-    return $clean_route;
+    $clean_route = $this->clean_route($route);
+    return $this->prefix_path . $clean_route;
+  }
+
+  private function append_method_to_route($route) {
+    return strtolower($this->request->request_method) . $route;
   }
 }
